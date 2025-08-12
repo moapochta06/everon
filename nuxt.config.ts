824a8f4c2fd81b36deed0cd122fb8dcacd5fb224
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2025-08-12 17:12:14 +0700

    added fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/image'],
+  css: [
+    '~/assets/css/styles.css',
+    '~/assets/css/fonts.css'
+  ],
 })
